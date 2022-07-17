5d8e75950785b459c3252bf85d06445525a37f24
Sumith Parambat Damodaran <sumithpd@gmail.com>
2022-07-17 15:57:27 +0100

    Update and rename BecomeNentor.js to BecomeMentor.js

--- src/src/components/BecomeMentor.js
+++ src/src/components/BecomeMentor.js
@@ -7,7 +7,7 @@ const BecomeMentor = () => {
                 <div className="row">
                     <div className="col-lg-6">
                         <div className="become-mentor__content">
-                            <h2 className="become-mentor__title">Teaching benefits</h2>
+                            <h2 className="become-mentor__title">Mentoring benefits</h2>
                             <p className="become-mentor__text">Lorem Ipsum is simply dummy text of the printing and
                                 type industry.
                                 Lorem Ipsum has been the standard dummy text ever since the when an unknown was
