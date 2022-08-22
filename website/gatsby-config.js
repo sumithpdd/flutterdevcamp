34ac76535f94ce6465213cf8c3ee6e294e0afa59
Sumith Damodaran <sumithpd@gmail.com>
2022-08-22 14:41:01 +0100

    added cloudnary with cms.js

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -65,8 +65,13 @@ module.exports = {
         ],
       },
     },
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    `gatsby-plugin-sass`, 
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
